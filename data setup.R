0b149c171fa547cd30c149d67c2df13f49a34e87
Kate Richerson <kate.e.richerson@noaa.gov>
2021-12-14 20:56:54 -0800

    Add in IOPAC ports locations, remove NA hauls

--- data setup.R
+++ data setup.R
@@ -16,6 +16,7 @@ load_data(c("WCGOP_proc_full", "EM_proc", "ASHOP_proc"))
 #Pulling out fields we MAY want at some point - some of these probably not needed
 ob_gf <- OBOrig_Proc_Full %>% 
   clean_names() %>% 
+  filter(!is.na(haul_id)) %>% 
   filter(sector %in% c("Limited Entry Trawl", 
                        "Catch Shares",
                        "Midwater Hake",
@@ -41,6 +42,7 @@ ob_gf <- OBOrig_Proc_Full %>%
 
 em_gf<- EMOrig_Proc %>% 
   clean_names() %>% 
+  filter(!is.na(haul_id)) %>% 
   mutate(sector2 = ifelse(sector == "Midwater Rockfish EM", "Midwater Rockfish", NA),
          sector2 = ifelse(sector == "Midwater Hake EM", "Shoreside/midwater Hake", sector2),
          sector2 = ifelse(sector == "Catch Shares EM" & gear %in% c("Bottom Trawl", "Midwater Trawl") , "LE/CS Trawl", sector2)) %>% 
@@ -55,27 +57,39 @@ em_gf<- EMOrig_Proc %>%
 
 ashop_gf <- ASOrig_Proc %>% 
   clean_names() %>% 
+  filter(!is.na(haul_id)) %>% 
   mutate(sector2 = ifelse(sector == "CATCHER-PROCESSOR", "At-sea hake CP", "At-sea hake MS")) %>% 
   select(year, sector, sector2, drvid,  gear, haul_id, set_date = deployment_date, up_date = retrieval_date, set_lat = latdd_start, set_long = londd_start, up_lat = latdd_end, up_long = londd_end, avg_lat, avg_long, avg_depth = depth, duration_in_min, spid_eqv, scientific_name, ret_mt, dis_mt, gfr_mt, tgt_mt) %>% 
   mutate(mt = dis_mt + ret_mt) %>% 
   mutate(haul_duration = duration_in_min/60) %>%
   select(-duration_in_min) %>% 
   mutate(data_source = "ASHOP") %>% 
-  mutate(drvid = as.character(drvid))
+  mutate(drvid = as.character(drvid)) %>% 
+  mutate(r_port = "AT SEA - NO RETURN PORT")
 
 
 gf <- bind_rows(ob_gf, em_gf, ashop_gf)
 
 #we probably need the data on the haul, rather than species/group level
 #There is a small but non-zero possibility that a haul or trip ID could be repeated across different data sources, so I am appending with the data source to be sure they are unique identifiers
-gf_haul <- gf %>% 
+gf_haul0 <- gf %>% 
   group_by(data_source, year, sector2, drvid, gear, r_port, r_date, trip_id, haul_id, set_date, set_lat, set_long) %>% 
   summarise(ret_mt= sum(ret_mt, na.rm=T)) %>% 
   ungroup() %>% 
   mutate(j_set_day = yday(set_date)) %>% 
   mutate(haul_id = paste0(data_source, haul_id),
          trip_id = ifelse(data_source == "ASHOP", NA, paste0(data_source, trip_id)))
-  
+
+#Add in return port lat/long
+r_ports <- read_csv("~/observer/Input/Richerson/iopac_ob_conversion_table.csv") %>% 
+  clean_names() %>% 
+  select(r_port_lat = pacfin_lat, r_port_long = pacfin_long, r_port) %>% 
+  filter(!is.na(r_port))
+
+gf_haul <- gf_haul0 %>% 
+  left_join(r_ports, by = "r_port")
+
+nrow(gf_haul) == nrow(gf_haul0)
 
 saveRDS(gf_haul, "~/observer/Input/Richerson/groundfish_spatial/gf_haul.rds")
 
@@ -94,7 +108,9 @@ fields <- data.frame(column_name = names(gf_haul),
                                     "Set latitude (decimal degrees)",
                                     "Set longitide (decimal degrees)",
                                     "Retained catch (metric tons)",
-                                    "Julian set day"),
+                                    "Julian set day",
+                                    "Latitude of return port",
+                                    "Longitude of return port"),
                      currently_used = c(F, 
                                         T, 
                                         T, 
@@ -102,12 +118,14 @@ fields <- data.frame(column_name = names(gf_haul),
                                         F, 
                                         T, 
                                         F, 
-                                        F, 
-                                        F, 
-                                        F, 
                                         T, 
                                         T, 
                                         T, 
+                                        T, 
+                                        T, 
+                                        T, 
+                                        T, 
+                                        T,
                                         T)
                      ) #set_date class is actually "POSIXct" "POSIXt" but since the former inherits from the latter I think this works
 
