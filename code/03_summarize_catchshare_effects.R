ab8f9fe5683b94788cc9999a08e77c9fd5bceb7f
Eric Ward <eric.ward@noaa.gov>
2022-07-29 06:14:41 -0700

    fix summaries to deal with all combos

--- code/03_summarize_catchshare_effects.R
+++ code/03_summarize_catchshare_effects.R
@@ -3,24 +3,44 @@ library(dplyr)
 library(gratia)
 library(ggplot2)
 
-data <- c("Alaska", "WC")[2]
-scale = c("region","port")[2]
+# include_all_vessels isn't included as a flag here because we'll just make
+# one summary for each cs sensitivity, binding results together below
+all_combos <- expand.grid(data = c("Alaska", "WC")[1],
+                          scale = c("port"),
+                          split_wc = c("north_south","one_area")[1],
+                          cs_sensitivity = c(TRUE,FALSE))
 
-#Split west coast into north/south of 40 10?
-split_wc <- c("north_south", "one_area")[1]
+for(ii in 1:nrow(all_combos)) {
+  data <- all_combos$data[ii]
+  scale <- all_combos$scale[ii]
+  #Split west coast into north/south of 40 10?
+  split_wc <- all_combos$split_wc[ii]
+  cs_sensitivity <- all_combos$cs_sensitivity[ii]
 
+  #label that appends file names with whether we subset only to vessels present both before and after CS ("stayers")
+  if(cs_sensitivity)
+  {
+    cs_sens_label = "stayers"
+  }else{
+    cs_sens_label = "allvessels"
+  }
+
+  model_results_all <- readRDS(paste0("output/aic_",scale,"_",data,"_",cs_sens_label,"_", "allvessels","_",split_wc,".rds"))
+  model_results_single <- readRDS(paste0("output/aic_",scale,"_",data,"_",cs_sens_label,"_", "singleport","_",split_wc,".rds"))
+  model_results <- rbind(model_results_all, model_results_single)
 # if(data == "Alaska"){
 #   best_model_index = c(5,5,5,5,6,6,5,5,6,6) #
 # }else{
 #   best_model_index = c((model_results %>% group_by(Run) %>% filter(AIC == min(AIC)))$Model)
 # }
-best_model_index = c((model_results %>% dplyr::group_by(Run) %>% dplyr::filter(AIC == min(AIC)))$Model) 
+best_models <- model_results %>%
+    dplyr::group_by(Run) %>%
+    dplyr::filter(AIC == min(AIC)) %>%
+    dplyr::group_by(Run, Scale,Metric,Area) %>%
+    dplyr::summarise(Model = Model[1])
+best_model_index = c(best_models$Model)
 
-run_names = c("inertia","inertia-ind",
-              "haul_dist","haul_dist-ind",
-              "eff_days","eff_days-ind",
-              "lat","lat-ind",
-              "lon","lon-ind")
+run_names = best_models$Run
 
 # Separate data frames for each sector
 df_1 = data.frame(Model = 1:length(run_names),
@@ -30,7 +50,7 @@ df_2 = data.frame(Model = 1:length(run_names),
 
 for(run in 1:length(run_names)) {
   # save models for later summarizing
-  fits = readRDS(paste0("output/gams_",scale, "_",run_names[run],"_",data,"_", split_wc,".rds"))
+  fits = readRDS(paste0("output/gams_",scale, "_",run_names[run],"_",data,"_",cs_sens_label,"_", split_wc,".rds"))
   df_1$Est[run] = summary(fits[[best_model_index[run]]])$p.coeff[2]
   df_1$SE[run] = summary(fits[[best_model_index[run]]])$se[2]
   df_1$P_value[run] = summary(fits[[best_model_index[run]]])$p.pv[2]
@@ -40,9 +60,9 @@ for(run in 1:length(run_names)) {
   df_2$P_value[run] = summary(fits[[best_model_index[run]]])$p.pv[3]
 }
 
-df_1$Run = c("Inertia (aggregate)", "Inertia (individual)",
-           "Distance (aggregate)", "Distance (individual)",
-           "Days (aggregate)", "Days (individual)",
+df_1$Run = c("Days (aggregate)", "Days (individual)",
+             "Distance (aggregate)", "Distance (individual)",
+            "Inertia (aggregate)", "Inertia (individual)",
            "Latitude (aggregate)", "Latitude (individual)",
            "Longitude (aggregate)", "Longitude (individual)")
 df_2$Run <- df_1$Run
@@ -64,3 +84,5 @@ df_table <- df %>%
   mutate_if(is.numeric, round, 4)
 
 write.csv(df_table,paste0("output/table_catchshares_",data,"_",cs_sens_label,".csv"), row.names = F)
+
+}
