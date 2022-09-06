ed890bffc970b3e69cd61b92bf0645c5e3b611d8
Eric Ward <eric.ward@noaa.gov>
2022-09-06 12:52:56 -0700

    update plots for meeting

--- figure_code/Figure_8.R
+++ figure_code/Figure_8.R
@@ -1,3 +1,4 @@
+library(viridis)
 
 data <- c("Alaska", "WC")[1]
 cs_sensitivity <- c(TRUE,FALSE)[2]
@@ -15,6 +16,28 @@ df_table <- read.csv(paste0("output/table_catchshares_",data,"_",cs_sens_label,"
 df_table$Scale <- "Aggregate"
 df_table$Scale[grep("individual", df_table$Run)] <- "Individual"
 df_table$Metric <- unlist(lapply(strsplit(df_table$Run," "), getElement, 1))
+df_table$Sector <- paste0("Alaska ", df_table$Sector)
+df_ak <- df_table
+
+data <- c("Alaska", "WC")[2]
+cs_sensitivity <- c(TRUE,FALSE)[2]
+if(cs_sensitivity)
+{
+  cs_sens_label = "stayers"
+}else{
+  cs_sens_label = "allvessels"
+}
+
+df_table <- read.csv(paste0("output/table_catchshares_",data,"_",cs_sens_label,".csv"))
+# parse run name
+df_table$Scale <- "Aggregate"
+df_table$Scale[grep("individual", df_table$Run)] <- "Individual"
+df_table$Metric <- unlist(lapply(strsplit(df_table$Run," "), getElement, 1))
+df_table$Sector <- paste0("West coast ", df_table$Sector)
+
+# join AK / WC data
+df_table <- rbind(df_ak, df_table)
+
 
 # make caterpillar plot of the metrics v catch share effects
 g1 <- ggplot(df_table, aes(Metric, Est,col=Scale)) +
@@ -25,5 +48,6 @@ g1 <- ggplot(df_table, aes(Metric, Est,col=Scale)) +
   xlab("") + ylab("") +
   theme_bw() +
   theme(strip.background =element_rect(fill="white")) +
-  coord_flip()
-ggsave(g1, filename = paste0("figures/catchshare_effects",data,"_",cs_sens_label,".png"))
+  coord_flip() +
+  scale_color_viridis_d(end=0.8)
+ggsave(g1, filename = paste0("figures/catchshare_effects",cs_sens_label,".png"))
